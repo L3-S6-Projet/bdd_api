0befdbdf69e549a763939ec553ccfc5f941ee57a
Yashovardhan Thevalil <yashthevalil@gmail.com>
2020-05-06 14:37:49 +0200

    Auth OK

--- scolendar/viewsets/auth_viewsets.py
+++ scolendar/viewsets/auth_viewsets.py
@@ -42,7 +42,7 @@ class AuthViewSet(ObtainAuthToken, TokenHandlerMixin):
                         )
                     }, required=['status', 'token', 'user', ])),
             403: Response(
-                description='Unauthorized access',
+                description='Invalid credentials (code=`InvalidCredentials`)',
                 schema=Schema(
                     title='ErrorResponse',
                     type=TYPE_OBJECT,
@@ -110,7 +110,7 @@ class AuthViewSet(ObtainAuthToken, TokenHandlerMixin):
                 )
             ),
             403: Response(
-                description='Invalid credentials (code=`InvalidCredentials`)',
+                description='Invalid token (code=`InvalidCredentials`)',
                 schema=Schema(
                     title='ErrorResponse',
                     type=TYPE_OBJECT,
